6ecf55718f9bca73e86d121705cf3bbb66e032b0
NYPM <josephrgoodman@gmail.com>
2025-10-14 21:17:35 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,24 @@
-const CACHE = 'fatloss-app-v1';
+const CACHE = 'fatloss-app-v3';
 const ASSETS = [
   './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.svg',
-  './icons/icon-512.svg',
+  './index.html?v=3',
+  './manifest.webmanifest?v=3',
+  './icons/icon-192.svg?v=3',
+  './icons/icon-512.svg?v=3',
   'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(r => r || fetch(e.request))
+  );
 });
