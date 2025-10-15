588a104f19fd422ef679e9bd489002fd8f12f2cb
NYPM <josephrgoodman@gmail.com>
2025-10-14 23:10:34 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,12 @@
-const CACHE = 'fatloss-app-v3-2';
+const CACHE = 'fatloss-app-v3-4';
 const ASSETS = [
   './',
-  './index.html?v=3.2',
-  './manifest.webmanifest?v=3.2',
-  './icons/icon-192.svg?v=3.2',
-  './icons/icon-512.svg?v=3.2',
+  './index.html?v=3.4',
+  './manifest.webmanifest?v=3.4',
+  './icons/icon-192.svg?v=3.4',
+  './icons/icon-512.svg?v=3.4',
   'https://cdn.jsdelivr.net/npm/chart.js'
 ];
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request, { ignoreSearch: true }).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request, { ignoreSearch: true }).then(r => r || fetch(e.request))); });
