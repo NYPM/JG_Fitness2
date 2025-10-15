528d3bf5da8152830d6f47d2c02ddd78772775af
NYPM <josephrgoodman@gmail.com>
2025-10-14 20:48:13 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'fatloss-app-min-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg','https://cdn.jsdelivr.net/npm/chart.js'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
